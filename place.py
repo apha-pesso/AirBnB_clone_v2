b5368380185d119516584a2d7402e1453ffc979f
MissTipo <dorine.a.tipo@gmail.com>
2022-12-07 18:59:05 +0300

    Update classes

---
+++ place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey, Column, Integer, String, Float
+
+
+class Place(BaseModel):
+    """ A place to stay """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
