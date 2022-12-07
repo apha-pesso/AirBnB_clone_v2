0bd73ea85bf19572ea54b68127e80bc862fc3d55
MissTipo <dorine.a.tipo@gmail.com>
2022-12-07 22:05:01 +0300

    update user

--- models/user.py
+++ models/user.py
@@ -8,8 +8,22 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+
+    """
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship('Place', cascade='delete', backref='user')
+    """
