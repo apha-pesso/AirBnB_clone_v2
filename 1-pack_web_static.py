65dbc776a97a0c36ac5d4344f7e1a4d71762c639
apha-pesso <mutiuayo99@gmail.com>
2022-12-23 00:28:34 +0000

    testing123...

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from
+the contents of the web_static folder"""
+from datetime import datetime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    local('mkdir -p versions')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(time)
+    file_name = "versions/{}".format(name)
+    location = local('tar -cvzf file_name web_static')
+    if location:
+        return (location)
+    else:
+        return None
