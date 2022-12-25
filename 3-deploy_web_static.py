04ec7e0f402c05fe61696b239e5c1c597d8beaff
apha-pesso <mutiuayo99@gmail.com>
2022-12-25 10:41:46 +0000

    Check output

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,20 +1,67 @@
 #!/usr/bin/python3
-"""Fabric script for full deployment"""
-from fabric.api import *
+"""Fabric script that generates a .tgz archive from
+the contents of the web_static folder
+and uploads and deploy on servers"""
+from fabric.api import local
 from datetime import datetime
-# Import the do_pack and do_deploy function
-packer = __import__('1-pack_web_static').do_pack
-deployer = __import__('2-do_deploy_web_static').do_deploy
+
+
 env.hosts = ['100.25.31.96', "107.23.94.25"]
-# env.hosts = ['100.25.31.96']
 
 
-def deploy():
-    """Fabric script that creates and distributes an archive
-    to your web servers"""
-    # call the do_pack function
-    path = packer()
-    if not path:
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    local('mkdir -p versions')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(time)
+    file_name = "versions/{}".format(name)
+    local("tar -cvzf {} web_static/".format(file_name))
+    if file_name:
+        return (file_name)
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploy a .tgz archive from the contents
+    of the web_static to the server"""
+    if not archive_path:
         return False
-    ret = deployer(path)
-    return (ret)
+    # Upload to /tmp/ in the server
+    # put(archive_path, "/tmp/")
+    with cd("/tmp/"):
+        res = put(archive_path, "/tmp/")
+        print(res)
+
+    # Make directory for the file extraction
+    run("mkdir -p /data/web_static/releases/")
+
+    # GEt the file name
+    file_name = archive_path.split("/")[-1]
+
+    # Extract the file from archive
+    remote_name = file_name.split(".")[0]
+    run("mkdir -p /data/web_static/releases/{}".format(remote_name))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        .format(file_name, remote_name))
+
+    # Delete the archive from the web serve
+    run("rm /tmp/{}".format(file_name))
+
+    # Move file out of web_static
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(remote_name, remote_name))
+
+    # Delete the symbolic link
+    run("rm -rf /data/web_static/current")
+
+    # Create a new the symbolic link
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(remote_name))
+    return (True)
+
+    def deploy():
+        """Execute the functions to create archive and deploy"""
+        path = do_pack()
+        ret = do_deploy(path)
+        return (ret)
