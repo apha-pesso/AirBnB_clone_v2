aabffac6d1dbcad01151f1b27c5ab328b19aa526
apha-pesso <mutiuayo99@gmail.com>
2023-01-15 09:35:12 +0000

    List all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models.__init__ import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def get_list():
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """Teardown method to close the db"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
