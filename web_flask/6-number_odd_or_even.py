767bd9c8c8e0f5d149947cb4c38eded2baf35af5
apha-pesso <mutiuayo99@gmail.com>
2023-01-15 09:34:49 +0000

    Odd or even route

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# A script that starts a Flask web application
+
+from flask import render_template, Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Function that prints Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index_hbnb():
+    """Function that prints Hello HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def index_c(text):
+    """Function that prints Hello HBNB"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def index_python(text):
+    """Function that prints Hello HBNB"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def index_number(n):
+    """Function that prints Hello HBNB"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def index_number_template(n):
+    """Function that prints Hello HBNB"""
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def index_number_odd_or_even(n):
+    """Function that prints Hello HBNB"""
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
